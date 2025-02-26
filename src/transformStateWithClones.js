8e96b122a2c9aeee2969c257bb951289a3c2219b
DrDakka <drdakkadakka@gmail.com>
2025-02-26 16:49:38 +0100

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,37 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  function addProps(state, extraData) {
+    Object.assign(state, extraData);
+  }
+
+  function removeProps (state, keysToRemove) {
+    for (const key of keysToRemove) {
+      delete state[key];
+    }
+  }
+
+  function clear (state) {
+    for (const i of state) {
+      delete state[i];
+    }
+  }
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        addProps(state, action.extraData);
+        break;
+
+      case 'removeProperties':
+        removeProps(state, keysToRemove);
+        break;
+
+      case 'clear':
+        clear(state);
+        break;
+    }
+  }
 }
 
 module.exports = transformStateWithClones;
